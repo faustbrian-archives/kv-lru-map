be250fa6e383f48aea82c6d27a5947f2868ff9ef
Brian Faust <hello@basecode.sh>
2019-05-13 05:45:29 +0300

    wip

--- __tests__/async.test.ts
+++ __tests__/async.test.ts
@@ -1,7 +1,7 @@
 import { complianceTestsAsync } from "@keeveestore/test-suite";
 import { StoreAsync } from "../src/async";
 
-complianceTestsAsync(new StoreAsync<string, string>(32), {
+complianceTestsAsync(() => StoreAsync.new<string, string>(32), {
 	key1: "value1",
 	key2: "value2",
 	key3: "value3",
